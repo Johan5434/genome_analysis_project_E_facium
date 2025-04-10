01e531b67e9e219331ee643f875d1d3ec9cc602c
Johan Eliasson <joel5434@rackham1.uppmax.uu.se>
2025-04-10 09:22:52 +0200

    restarting the git completely t push -u origin main :wq i

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # genome_analysis_project_E_facium
 This is the second version of my original repository. Had to do a complete reset. 
+=======
+# genomanalys_project
+>>>>>>> b355980 (restarting the git completely)
---
+++ code/genome_assembly_scripts/genome_assembly_illumina/illumina_assembly_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH -t 06:00:00
+#SBATCH -J spades_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user eliassonjohan1@gmail.com
+
+# Ladda först bioinfo-tools, sedan SPAdes
+module load bioinfo-tools
+module load spades/3.15.3
+
+output_dir="/home/joel5434/genomanalys_project/analyses/02_genome_assembly/Illumina_and_Nanopore_assembly_result"
+mkdir -p ${output_dir}
+
+spades.py \
+  --pe1-1 /home/joel5434/genomanalys_project/code/genome_assembly_scripts/genome_assembly_illumina/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz \
+  --pe1-2 /home/joel5434/genomanalys_project/code/genome_assembly_scripts/genome_assembly_illumina/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz \
+  --nanopore /home/joel5434/genomanalys_project/code/genome_assembly_scripts/genome_assembly_illumina/Nanopore/E745_all.fasta.gz \
+  --careful \
+  --cov-cutoff auto \
+  -k 21,33,55,77,99,127 \
+  -o ${output_dir}
+
