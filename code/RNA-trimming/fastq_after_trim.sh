8c4a9aae1f8b1e377da92c6388598fc919ed147c
Johan Eliasson <joel5434@rackham3.uppmax.uu.se>
2025-05-11 20:12:53 +0200

    looooaaads of stuff

--- code/RNA-trimming/fastq_after_trim.sh
+++ code/RNA-trimming/fastq_after_trim.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 3
+#SBATCH -n 2
 #SBATCH -t 04:00:00
 #SBATCH -J RNA_FastQC
 #SBATCH --mail-type=ALL
@@ -11,15 +11,23 @@
 
 module load bioinfo-tools FastQC
 
-TRIMMED_FILES=/home/joel5434/genomanalys_project/analyses/07_rna_trimming/trimmed_reads/*.gz
 
-OUT_DIR=/home/joel5434/genomanalys_project/analyses/07_rna_trimming
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+CATEGORY="$3"
 
-mk -p $OUT_DIR/fastqc_check_after
-mkdir -p $OUT_DIR/fastqc_check_serum_before
+if [ $# -ne 3 ]; then
+  echo "Usage: sbatch $0 <input_dir> <output_root_dir> <category>"
+  exit 1
+fi
 
-fastqc -t $SLURM_NTASKS $RAW_FILES_BH \
-       -o $OUT_DIR/fastqc_check_BH_before
+CATEGORY_DIR="$OUTPUT_DIR/$CATEGORY"
+mkdir -p "$CATEGORY_DIR"
 
-fastqc -t $SLURM_NTASKS $RAW_FILES_SERUM \
-       -o $OUT_DIR/fastqc_check_serum_before
+for fq in "$INPUT_DIR"/*.fastq.gz; do
+basename=$(basename "$fq" | cut -d'_' -f1)
+sample_dir="$CATEGORY_DIR/$basename"
+mkdir -p "$sample_dir"
+
+fastqc -t 2 "$fq" -o "$sample_dir" 
+done 
